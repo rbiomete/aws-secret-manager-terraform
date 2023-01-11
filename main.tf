e89f0012ab8dc072e66ccbb007f04f20a2ce56c6
rbiomete <100229496+rbiomete@users.noreply.github.com>
2023-01-11 20:47:59 +0400

    edit 4

--- main.tf
+++ main.tf
@@ -230,9 +230,9 @@ resource "aws_kms_alias" "secret" {
 
 
 
-resource "aws_secretsmanager_secret_rotation" "secret" {
+resource "aws_secretsmanager_secret" "secret" {
   description         = var.secret_description
-  secret_id          = aws_secretsmanager_secret.secret.id
+  kms_key_id          = aws_kms_key.secret.key_id
   name                = var.name
   rotation_lambda_arn = aws_lambda_function.rotate-code-mysql.arn
   rotation_rules {
@@ -244,18 +244,20 @@ resource "aws_secretsmanager_secret_rotation" "secret" {
 
 resource "aws_secretsmanager_secret_version" "secret" {
   lifecycle {
-    ignore_changes = [secret_string]
+    ignore_changes = [
+      secret_string
+    ]
   }
-  secret_id     = aws_secretsmanager_secret.secret.id
+  secret_id     = "${aws_secretsmanager_secret.secret.id}"
   secret_string = <<EOF
 {
-  username: "${var.mysql_username}",
-  engine: mysql,
-  dbname: "${var.mysql_dbname}",
-  host: "${var.mysql_host}",
-  password: "${var.mysql_password}",
-  port: ${var.mysql_port},
-  dbInstanceIdentifier: "${var.mysql_dbInstanceIdentifier}"
+  "username": "${var.mysql_username}",
+  "engine": "mysql",
+  "dbname": "${var.mysql_dbname}",
+  "host": "${var.mysql_host}",
+  "password": "${var.mysql_password}",
+  "port": ${var.mysql_port},
+  "dbInstanceIdentifier": "${var.mysql_dbInstanceIdentifier}"
 }
 EOF
 }
